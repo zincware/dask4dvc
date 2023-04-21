0c36513248944159b68609af82ac828b95e11c70
PythonFZ <fabian.zills@web.de>
2023-04-21 12:05:44 +0200

    allow -f/--force

--- dask4dvc/cli/main.py
+++ dask4dvc/cli/main.py
@@ -41,7 +41,7 @@ def repro(
     config: str = typer.Option(None, help=Help.config),
     max_workers: int = typer.Option(None, help=Help.max_workers),
     retries: int = typer.Option(10, help=Help.retries),
-    force: bool = typer.Option(False, help="use `dvc repro --force`"),
+    force: bool = typer.Option(False, "-f/--force", help="use `dvc repro --force`"),
 ) -> None:
     """Replicate 'dvc repro' command using dask."""
     utils.CONFIG.retries = retries
